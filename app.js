c9d876574d6d893d6578a16295b1a0ad8c9ed511
mehakbawany <mehakbawany2009@gmail.com>
2026-08-07 16:08:43 +0500

    gsapedit

---
+++ app.js
@@ -0,0 +1,58 @@
+console.log("jsloaded")
+let tween = gsap.to("#heading" ,{
+text :  {value:"Welcome to Our Foodie's Hub Restaurant"},
+duration: 3,
+delay: 1,
+ease:SteppedEase.config(24),
+})
+console.log("jsloaded")
+
+ gsap.to("#heading2" ,{
+text :  {value:"Delicious Food Made With Passion"},
+duration: 3,
+delay: 4,
+ease:SteppedEase.config(24),
+})
+
+//   gsap.to("#foodcard" ,{
+//     text :  {value:""},
+//     duration: 3,
+//     delay: 1,
+//     ease:SteppedEase.config(24),
+//     })
+    gsap.to("#foodcard1" ,{
+        text :  {value:"Cheese Burger"},
+        duration: 3,
+        delay: 7,
+        ease:SteppedEase.config(24),
+        })
+        gsap.to("#foodcard2" ,{
+            text :  {value:"Italian Pizza"},
+            duration: 3,
+            delay: 7,
+            ease:SteppedEase.config(24),
+            })
+            gsap.to("#foodcard3" ,{
+                text :  {value:"Grilled Steak"},
+                duration: 3,
+                delay: 7,
+                ease:SteppedEase.config(24),
+                })
+                gsap.to("#foodcard4" ,{
+                    text :  {value:"Creamy Pasta"},
+                    duration: 3,
+                    delay: 7,
+                    ease:SteppedEase.config(24),
+                    })
+                    gsap.to("#foodcard5" ,{
+                        text :  {value:"Creamy Pasta"},
+                        duration: 3,
+                        delay: 7,
+                        ease:SteppedEase.config(24),
+                        })
+                        gsap.to("#foodcard6" ,{
+                            text :  {value:"Creamy Pasta"},
+                            duration: 3,
+                            delay: 7,
+                            ease:SteppedEase.config(24),
+                            })
